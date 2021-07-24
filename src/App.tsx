6aa52d16436b2526ad136efd04c80b06d881ceaa
vgTaranovsky <taranovscki.vadim@yandex.ru>
2021-07-24 19:38:06 +0400

    feat(main): add more tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import * as pureJs from "./pureJsTasks";
 
 export const App = (): JSX.Element => {
-  console.log(pureJs.myChunk([1, 2, 3, 4, 5, 6, 7, 8, 8, 9], 3));
   return <div>This app for learn new js stuff</div>;
 };
--- src/pureJsTasks/index.ts
+++ src/pureJsTasks/index.ts
@@ -33,3 +33,119 @@ export const myChunk = (arr: Array<any>, length: number): Array<any[]> => {
 };
 
 // Task 3
+
+const sum = (a: number, b: number): number => a + b;
+
+const myPartial = <S>(callback: (...rest: S[]) => any, constant: S) => {
+  return (val: S) => {
+    return callback.call(null, constant, val);
+  };
+};
+
+export const sumTwo = myPartial<number>(sum, 2);
+
+// Task 4
+
+const logInfo = <S>(arr: S[]): string =>
+  arr.reduce((acc, item) => {
+    acc += item + " ";
+    return acc;
+  }, "");
+
+const withDelay = <S>(
+  callback: typeof logInfo,
+  delay: number,
+  ...rest: S[]
+) => {
+  return (...args: S[]) => {
+    return new Promise((resolve, reject) => {
+      const timer = window.setTimeout(() => {
+        resolve(callback<S>(rest.concat(args)));
+        window.clearTimeout(timer);
+      }, delay);
+    });
+  };
+};
+
+export async function start() {
+  try {
+    const delaiedFunction = await withDelay<string>(logInfo, 2000, "Hello");
+    const result = await delaiedFunction("world");
+    console.log(result);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+// Task 5
+
+const tree = {
+  left: {
+    left: {},
+    right: {
+      value: 4,
+    },
+    value: 1,
+  },
+  right: {
+    left: {
+      left: {
+        left: {},
+        right: {
+          value: 5,
+        },
+        value: 2,
+      },
+      right: {},
+    },
+    right: {
+      value: 7,
+    },
+  },
+  value: 4,
+};
+
+const getSumValues = (obj: any): number => {
+  let result = obj.value || 0;
+
+  if (obj.left) {
+    result += getSumValues(obj.left);
+  }
+  if (obj.right) {
+    result += getSumValues(obj.right);
+  }
+
+  return result;
+};
+
+// Task 6
+
+export const canDrink = (person: { age: number }) => {
+  if (!person.age) {
+    throw new Error("Age does not exist");
+  }
+  const getDrinkResponse = (age: number) => {
+    if (age < 18) return "can not drink";
+    if (age < 21) return "can not drink in USA";
+    return "can drink";
+  };
+
+  return getDrinkResponse(person.age);
+};
+
+// Task 7
+const multiArr = [1, 2, [1, 2, 3, [4, 5], [6]], [8, 5, 6, [1, [2, 3, [6]]]]];
+
+export const myFlat = (arr: any[]): any[] => {
+  let result: any[] = [];
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      result = [...result, ...myFlat(item)];
+    } else {
+      result = [...result, item];
+    }
+  }
+  return result;
+};
+
+// Task 8
