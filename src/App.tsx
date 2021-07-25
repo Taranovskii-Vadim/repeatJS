ba271a1b99827fd5e3273310e7654350e7c3f251
VGTaranovsky <taranovscki.vadim@yandex.ru>
2021-07-25 13:26:29 +0400

    feat(main): add more tasks and tests

--- src/App.tsx
+++ src/App.tsx
@@ -3,5 +3,7 @@ import React from "react";
 import * as pureJs from "./pureJsTasks";
 
 export const App = (): JSX.Element => {
+  console.log(pureJs.isPalindrom("А роза упала на лапу Азора"));
+
   return <div>This app for learn new js stuff</div>;
 };
