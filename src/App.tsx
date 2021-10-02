26f8d7d051dd98cc8960f34e70fd7acd737564ec
vgTaranovsky <taranovscki.vadim@yandex.ru>
2021-10-02 11:41:27 +0400

    feat(main): add counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
+import Counter from "./components/Counter";
 import List from "./components/List";
 
 export const App = (): JSX.Element => {
-  return <List />;
+  return <Counter />;
 };
---
+++ src/components/Counter/index.tsx
@@ -0,0 +1,36 @@
+import React, { useCallback, useEffect, useState } from "react";
+
+const Buttons = React.memo(
+  ({ onPlus, onMinus }: { [key: string]: () => void }): JSX.Element => (
+    <>
+      <button onClick={onMinus}>-</button>
+      <button onClick={onPlus}>+</button>
+    </>
+  )
+);
+
+const Counter = (): JSX.Element => {
+  const [count, setCount] = useState(() => 0);
+
+  const [random, setRandom] = useState(() => 0);
+
+  useEffect(() => {
+    setRandom(Math.floor(Math.random() * 10));
+  }, [count]);
+
+  const onMinus = useCallback(() => setCount((prev) => prev - 1), []);
+
+  const onPlus = useCallback(
+    () => setCount((prev) => prev + 1 + random),
+    [random]
+  );
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <Buttons onMinus={onMinus} onPlus={onPlus} />
+    </div>
+  );
+};
+
+export default Counter;
