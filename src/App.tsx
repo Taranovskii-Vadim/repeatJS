354fa35cefab6eeaf4a47f3a75484777eb8ba3bb
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-03-18 17:57:04 +0400

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import { useEffect } from "react";
-import { myPromiseAll } from "./pureJsTasks";
+
+// Interview Questions example
+// 1) Diff between interfaces and types
+// 2) What is "as" in TypeScript
+// 3) What is interceptor in frontend. Where we can use it?
+// 4) We have status system. Frontend send PUT request with new status, should we await API response to render new status or not.
+// 5)
+
+// Interview Tasks example
+// 1) We have string and search string. Need create a function that can return JSX with string and substring highlighted
 
 // TODO первый вопрос про семантику в html и насколько она необходима если мы делаем SPA на реакте
 // TODO задача про поиск в табах
 // TODO задача на софт скиллы, что такое ревью, что если возникли ошибки в результате ревью, что если разработчик сделал мерж без ревью
-// TODO вопросы по interceptors
 // TODO createPortal
-// TODO task about hightlight substring in string
 // TODO diff between array and list
 // TODO question about unit tests, we have parent component and we have unit test, and we have child component but we dont have unit test but this component 100%
 // TODO what is em? rem? vh? vw?
-// TODO если у нас есть система статусов и мы обновляем статус то откуда мы должны брать новый статус?
 // TODO назвать способы подключения шрифтов в react проект
 // TODO задача с изменением цвета тега body при переходе из формы логина в продукт
-// TODO  tree-shaking what is it
-// TODO what is as in TS
-// TODO whats diff between types and interfaces
+// TODO tree-shaking what is it
 
 const App = (): JSX.Element => {
   return <div>hello vite</div>;
