c73e5ddf691293939c3bd7b5926f6ab0ef020320
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-11-03 13:35:57 +0400

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import CountryCapitalGame from './components/CountryCapitalGame';
 
+import UsersTable from './components/UsersTable';
+
 const App = (): JSX.Element => (
-  <CountryCapitalGame
-    data={{
-      germany: 'berlin',
-      russia: 'moscow',
-      italy: 'rome',
-      usa: 'washington',
-      portugal: 'lisbon',
-      china: 'bejin',
-      japan: 'tokyo',
-      vietnam: 'hanoi',
-    }}
-  />
+  <UsersTable />
+  // <CountryCapitalGame
+  //   data={{
+  //     germany: 'berlin',
+  //     russia: 'moscow',
+  //     italy: 'rome',
+  //     usa: 'washington',
+  //     portugal: 'lisbon',
+  //     china: 'bejin',
+  //     japan: 'tokyo',
+  //     vietnam: 'hanoi',
+  //   }}
+  // />
 );
 
 export default App;
--- src/components/UsersTable/index.tsx
+++ src/components/UsersTable/index.tsx
@@ -4,27 +4,61 @@ const API_URL = 'https://randomuser.me/api/?results=20';
 
 type SortDirection = 'asc' | 'desc';
 
+type ResponseDTO = {
+  cell: string;
+  dob: { date: string; age: number };
+  email: string;
+  gender: 'female' | 'male';
+  id: { name: string; value: string };
+  location: {
+    city: string;
+    coordinates: { latitude: string; longitude: string };
+    country: string;
+    postcode: number;
+    state: string;
+    street: { number: number; name: string };
+    timezone: { offset: string; description: string };
+  };
+  login: {
+    uuid: string;
+    username: string;
+    password: string;
+    salt: string;
+    md5: string;
+    sha1: string;
+    sha256: string;
+  };
+  name: { title: string; first: string; last: string };
+  nat: string;
+  phone: string;
+  picture: {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  };
+  registered: { date: string; age: number };
+};
+
 const UsersTable = () => {
-  const [state, setState] = useState([]);
   const sortRef = useRef<SortDirection>();
-  const [locations, setLocations] = useState([]);
+  const [locations, setLocations] = useState<Record<string, string | number>[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(API_URL);
-      const { results } = await response.json();
+      const { results } = (await response.json()) as { results: ResponseDTO[] };
 
-      const flat = (item: any) => {
-        const result = {};
+      const flat = (item: ResponseDTO['location']) => {
+        const result: Record<string, string | number> = {};
 
-        const solve = (node: any, prevKey?: string) => {
+        const solve = (node: Record<string, string | number | object>, prevKey?: string) => {
           for (let key in node) {
             const newKey = prevKey ? prevKey + '.' + key : key;
 
             if (typeof node[key] === 'object' && node[key] !== null) {
               solve(node[key], newKey);
             } else {
-              result[newKey] = node[key];
+              result[newKey] = node[key] as string | number;
             }
           }
         };
@@ -62,7 +96,6 @@ const UsersTable = () => {
       //   return result;
       // };
 
-      setState(results);
       setLocations(results.map(({ location }) => flat(location)));
     };
 
