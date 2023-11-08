0ea4d14c0be5b3f4cffa2df6faa286a5727fa41b
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-11-08 17:27:18 +0400

    add task

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { findTwoPositive } from './pureJsTasks';
+import { findHotterDay } from './tasks/abstract';
 
 const App = (): JSX.Element => {
+  console.log(findHotterDay([13, 12, 15, 11, 9, 12, 16]));
+
   return <div></div>;
 };
 
--- src/tasks/abstract/index.ts
+++ src/tasks/abstract/index.ts
@@ -1,6 +1,10 @@
 // Task 1
 // Дано: Массив батареек ([-1, 1, -1, 1, ....]), где -1 батарея разряжена, 1 батарея заряжена.
 // Найти: Необходимо найти минимальное количество попыток за которое человек может выбрать 2 заряженные батареи.
+
+// O(n)
+// O(1)
+
 export const findTwoPositive = (data: number[]): number => {
   let count = 0;
 
@@ -22,3 +26,46 @@ export const findTwoPositive = (data: number[]): number => {
 };
 
 // Task 2
+// Дано: Массив чисел обозначающих температуру в характерный день.
+// Найти: Для каждого элемента массива, через какое минимальное количество дней будет теплее
+
+export const findHotterDay = (data: number[]): number[] => {
+  const result: number[] = [];
+
+  // O(n ^ 2)
+  // O(1)
+
+  // for (let i = 0; i < data.length; i++) {
+  //   result[i] = 0;
+
+  //   for (let j = i + 1; j < data.length; j++) {
+  //     if (data[j] > data[i]) {
+  //       result[i] = j - i;
+  //       break;
+  //     }
+  //   }
+  // }
+
+  // O(n)
+  // O(n)
+
+  const stack: Array<{ value: number; index: number }> = [];
+
+  for (let i = data.length - 1; i >= 0; i--) {
+    result[i] = 0;
+
+    for (let j = stack.length - 1; j >= 0; j--) {
+      if (data[i] < stack[j].value) {
+        result[i] = stack[j].index - i;
+
+        break;
+      } else {
+        stack.pop();
+      }
+    }
+
+    stack.push({ value: data[i], index: i });
+  }
+
+  return result;
+};
