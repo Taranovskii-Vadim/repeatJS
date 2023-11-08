670575bae8d1f61cf960ba98aab4f5e0a1f39d97
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-11-08 23:52:16 +0400

    add base deepEqual function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
+import { deepEqual } from './tasks/technic';
+
 const App = (): JSX.Element => {
+  console.log(
+    deepEqual(
+      { name: 'vadim', age: 18, location: { street: 'lenina' } },
+      { name: 'vadim', age: 18, location: { street: 'lenina' } },
+    ),
+  );
   return <div></div>;
 };
 
--- src/tasks/technic/index.ts
+++ src/tasks/technic/index.ts
@@ -35,3 +35,44 @@ export const promiseAll = async <T>(data: Promise<T>[]): Promise<T[]> => {
     }
   });
 };
+
+// Task 2
+// Цель: Реализовать свой deepEqual
+
+export const deepEqual = <T>(first: T, second: T): boolean => {
+  if (first instanceof Set && second instanceof Set) {
+    if (first.size !== second.size) return false;
+
+    const firstValues = [...first.values()];
+    const secondValues = [...second.values()];
+
+    for (let i = 0; i < firstValues.length; i++) {
+      if (firstValues[i] !== secondValues[i]) return false;
+    }
+  }
+
+  if (typeof first === 'object' && typeof second === 'object') {
+    const firstKeys = Object.keys(first);
+    const secondKeys = Object.keys(second);
+
+    if (firstKeys.length !== secondKeys.length) return false;
+
+    for (let i = 0; i < firstKeys.length; i++) {
+      if (firstKeys[i] !== secondKeys[i]) return false;
+    }
+
+    for (let key of firstKeys) {
+      if (typeof first[key] === 'object' && typeof second[key] === 'object') {
+        const result = deepEqual(first[key], second[key]);
+
+        if (!result) return false;
+      } else if (first[key] !== second[key]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  return Object.is(first, second);
+};
