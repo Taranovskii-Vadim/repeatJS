b2ca28bd92bfdd6a78c5c3e307fce9afd2133504
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-11-21 15:47:47 +0400

    add my promiseAllSettled

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { useEffect } from 'react';
 import { findHIndex, findMaxResult, findSubArrays } from './tasks/abstract';
+import { promiseAllSettled } from './tasks/technic/promises';
 
-const first = new Promise((resolve) => {
+const first = new Promise<number>((resolve) => {
   setTimeout(() => resolve(2), 2000);
 });
 
-const second = new Promise((resolve) => {
-  setTimeout(() => resolve(1), 1000);
+const second = new Promise<number>((resolve, reject) => {
+  setTimeout(() => reject(1), 1000);
 });
 
-const third = new Promise((resolve) => {
+const third = new Promise<number>((resolve, reject) => {
   setTimeout(() => resolve(5), 5000);
 });
 
@@ -17,7 +19,17 @@ const App = (): JSX.Element => {
 
   // console.log(findSubArrays([4, 2, 2, 1, 2, -3, 5, -8], 5));
 
-  console.log(findHIndex([3, 0, 6, 1, 5]));
+  // console.log(findHIndex([3, 0, 6, 1, 5]));
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await promiseAllSettled([first, second, third]);
+
+      console.log(response);
+    };
+
+    fetchData();
+  }, []);
 
   return <div></div>;
 };
--- src/tasks/technic/promises.ts
+++ src/tasks/technic/promises.ts
@@ -1,15 +1,4 @@
 // Task 1
-// Цель: Реализовать свой Promise.race
-
-export const promiseRace = <D>(data: Promise<D>[]): Promise<D> => {
-  return new Promise((resolve, reject) => {
-    data.forEach((promise) => {
-      promise.then(resolve).catch(reject);
-    });
-  });
-};
-
-// Task 2
 // Цель: Реализовать свой класс Promise
 
 type Init<D> = (resolve: (value: D) => void) => void;
@@ -37,6 +26,17 @@ export class myPromise<D> {
     });
   }
 }
+
+// Task 2
+// Цель: Реализовать свой Promise.race
+
+export const promiseRace = <D>(data: Promise<D>[]): Promise<D> => {
+  return new Promise((resolve, reject) => {
+    data.forEach((promise) => {
+      promise.then(resolve).catch(reject);
+    });
+  });
+};
 
 // Task 3
 // Цель: Реализовать свой promiseAll
@@ -75,3 +75,37 @@ export const promiseAll = async <T>(data: Promise<T>[]): Promise<T[]> => {
     }
   });
 };
+
+// Task 4
+// Цель: Реализовать свой promiseAllSettled
+
+type ResolvedPromise<D> = { status: 'fulfilled'; data: D };
+type RejectedPromise = { status: 'rejected'; reason: string };
+
+type Response<D> = ResolvedPromise<D> | RejectedPromise;
+
+export const promiseAllSettled = <D>(data: Promise<D>[]): Promise<Response<D>[]> => {
+  const result: Response<D>[] = [];
+
+  let count = 0;
+
+  return new Promise((resolve) => {
+    data.forEach((promise, index) => {
+      promise
+        .then((response) => {
+          result[index] = { status: 'fulfilled', data: response };
+        })
+        .catch((e) => {
+          result[index] = { status: 'rejected', reason: e as string };
+        })
+        .finally(() => {
+          if (++count === data.length) {
+            resolve(result);
+          }
+        });
+    });
+  });
+};
+
+// Task 5
+// Цель: Реализовать свой promiseAny
